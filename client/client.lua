d2eaa310ee69c5687b0b02e4501f86f9e86d846b
Csoki <feketetibi71@gmail.com>
2023-03-18 12:30:02 +0100

    refactor(client/client.lua)
    - fix blips
    - removed unnecessary for loop (row: 7)

--- client/client.lua
+++ client/client.lua
@@ -1,80 +1,91 @@
 local isWashing = false
-local PlayerPed = PlayerPedId()
 
+local function createBlip(coord)
+    local blip = AddBlipForCoord(coord.x, coord.y, coord.z)
 
-for k,v in pairs(Config.CarWash.positions) do
-    carWash = {['carWash'..k] = lib.zones.box({coords = v.coord, size = v.size, rotation = v.rot, debug = v.debug, inside = inside, onEnter = onEnter, onExit = onExit})}
-    for _,i in pairs(carWash) do
-        SetBlipSprite(carWashBlip, 100)
-        SetBlipAsShortRange(carWashBlip, true)
-        BeginTextCommandSetBlipName("STRING")
-        AddTextComponentSubstringPlayerName(Config.CarWash.blipName)
-        EndTextCommandSetBlipName(carWashBlip)
-        EndTextCommandSetBlipName(carWashBlip)
-        function i:onEnter()
-            if IsPedInAnyVehicle(PlayerPed, false) then
-                if Config.useRadial then
-                    lib.addRadialItem({
-                        id = 'carwash',
-                        icon = Config.radialData.icon,
-                        label = Config.radialData.label,
-                        onSelect = function()
-                            TriggerEvent('fusti_carwash:CanWash')
-                        end
-                    })
-                    if Config.radialData.useNotify then
-                        lib.notify({
-                            title = Config.radialData.NotifyTitle,
-                            description = Config.radialData.NotifyDescription,
-                            type = Config.radialData.NotifyType,
-                            duration = Config.radialData.NotifyDuration,
-                        })
-                    end
-                else
-                    lib.notify({
-                        title = Config.CarWash.InformNotify.title,
-                        description = Config.CarWash.InformNotify.description..Config.CarWash.price..'$',
-                        type = Config.CarWash.InformNotify.type,
-                        duration = Config.CarWash.InformNotify.duration
-                    })
-                end
-            end
+    SetBlipSprite(blip, 100)
+    SetBlipAsShortRange(blip, true)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentSubstringPlayerName(Config.CarWash.blipName)
+    EndTextCommandSetBlipName(blip)
+
+    return blip
+end
+
+for _, v in pairs(Config.CarWash.positions) do
+    v.blip = createBlip(v.coord)
+
+    local zone = lib.zones.box({
+        coords = v.coord,
+        size = v.size,
+        rotation = v.rot,
+        debug = v.debug,
+    })
+    v.zone = zone
+
+    function zone:onEnter()
+        if not IsPedInAnyVehicle(cache.ped, false) then
+            return
         end
-        function i:inside()
-            if IsPedInAnyVehicle(PlayerPed, false) then
-                if not Config.useRadial then
-                    if IsControlJustReleased(0, 38) then
-                        TriggerEvent('fusti_carwash:CanWash')
-                    end
+        if Config.useRadial then
+            lib.addRadialItem({
+                id = 'carwash',
+                icon = Config.radialData.icon,
+                label = Config.radialData.label,
+                onSelect = function()
+                    TriggerEvent('fusti_carwash:CanWash')
                 end
+            })
+            if Config.radialData.useNotify then
+                lib.notify({
+                    title = Config.radialData.NotifyTitle,
+                    description = Config.radialData.NotifyDescription,
+                    type = Config.radialData.NotifyType,
+                    duration = Config.radialData.NotifyDuration,
+                })
             end
+            return
         end
-        function i:onExit()
+        lib.notify({
+            title = Config.CarWash.InformNotify.title,
+            description = Config.CarWash.InformNotify.description .. Config.CarWash.price .. '$',
+            type = Config.CarWash.InformNotify.type,
+            duration = Config.CarWash.InformNotify.duration
+        })
+    end
+
+    function zone:onExit()
+        if Config.useRadial then
             lib.removeRadialItem('carwash')
+        end
+    end
+
+    function zone:inside()
+        if not IsPedInAnyVehicle(cache.ped, false) then
+            return
+        end
+        if Config.useRadial then
+            return
+        end
+        if IsControlJustReleased(0, 38) then
+            TriggerEvent('fusti_carwash:CanWash')
         end
     end
 end
 
-RegisterNetEvent('fusti_carwash:CanWash')
 AddEventHandler('fusti_carwash:CanWash', function()
-    if not isWashing then
-        TriggerServerEvent('fusti_carwash:checkWash')
-    else
-        lib.notify({
+    if isWashing then
+        return lib.notify({
             title = Config.CarWash.ErrorNotify.title,
             description = Config.CarWash.ErrorNotify.description,
             type = Config.CarWash.ErrorNotify.type
         })
     end
+
+    TriggerServerEvent('fusti_carwash:checkWash')
 end)
 
-RegisterNetEvent('fusti_carwash:startWash')
-AddEventHandler('fusti_carwash:startWash', function()
-    startWash()
-end)
-
-RegisterNetEvent('fusti_carwash:notify')
-AddEventHandler('fusti_carwash:notify', function(title, description, icon)
+RegisterNetEvent('fusti_carwash:notify', function(title, description, icon)
     lib.notify({
         title = title,
         description = description,
@@ -84,7 +95,7 @@ end)
 
 function startWash()
     isWashing = true
-    local vehicle = GetVehiclePedIsIn(PlayerPed)
+    local vehicle = GetVehiclePedIsIn(cache.ped)
     local dist = 'cut_family2'
     local fxName = 'cs_fam2_ped_water_splash'
     FreezeEntityPosition(vehicle, true)
@@ -93,20 +104,22 @@ function startWash()
         Wait(1)
     end
     UseParticleFxAssetNextCall(dist)
-    local particle = StartParticleFxLoopedAtCoord(fxName, GetEntityCoords(PlayerPed), 0.0, 0.0, 0.0, 8.0, false, false, false, 0)
+    local particle = StartParticleFxLoopedAtCoord(fxName, GetEntityCoords(cache.ped), 0.0, 0.0, 0.0, 8.0, false,
+        false,
+        false, 0)
     if lib.progressCircle({
-        duration = Config.CarWash.progress.duration,
-        label = Config.CarWash.progress.label,
-        position = Config.CarWash.progress.position,
-        useWhileDead = false,
-        canCancel = false,
-        disable = {
-            car = true,
-            move = true,
-            combat = true,
-            mouse = true
-        }
-    })
+            duration = Config.CarWash.progress.duration,
+            label = Config.CarWash.progress.label,
+            position = Config.CarWash.progress.position,
+            useWhileDead = false,
+            canCancel = false,
+            disable = {
+                car = true,
+                move = true,
+                combat = true,
+                mouse = true
+            }
+        })
     then
         StopParticleFxLooped(particle, false)
         SetVehicleDirtLevel(vehicle, 0.0)
@@ -120,3 +133,4 @@ function startWash()
         })
     end
 end
+RegisterNetEvent('fusti_carwash:startWash', startWash)
