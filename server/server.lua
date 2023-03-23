06823354d7162ffbff3168136a324d711fc71c90
Füsti <118769783+FUst1ke@users.noreply.github.com>
2023-03-23 17:30:49 +0100

    fix and refactor
    from Csoki :)

--- server/server.lua
+++ server/server.lua
@@ -1,11 +1,13 @@
 RegisterServerEvent('fusti_carwash:checkWash')
 AddEventHandler('fusti_carwash:checkWash', function()
-    local price = Config.CarWash.price
-    local money = exports.ox_inventory:GetItem(source, 'money', nil, true)
-    if money >= price then
-        exports.ox_inventory:RemoveItem(source, 'money', price)
-        TriggerClientEvent('fusti_carwash:startWash', source)
-    else
-        TriggerClientEvent('fusti_carwash:notify', source, Config.CarWash.MoneyNotify.title, Config.CarWash.MoneyNotify.description, Config.CarWash.MoneyNotify.type)
+    local player <const> = source
+    local price <const> = Config.CarWash.price
+
+    local money = exports.ox_inventory:GetItem(player, 'money', nil, true)
+    if money < price then
+        return TriggerClientEvent('fusti_carwash:notify', player, Config.CarWash.MoneyNotify.title, Config.CarWash.MoneyNotify.description, Config.CarWash.MoneyNotify.type)
     end
+    exports.ox_inventory:RemoveItem(player, 'money', price)
+
+    TriggerClientEvent('fusti_carwash:startWash', player)
 end)
